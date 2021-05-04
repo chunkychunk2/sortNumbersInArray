d09bb5198a8adbf0b506c290d094a7c2d89e3554
chunkychunk2 <49397604+chunkychunk2@users.noreply.github.com>
2021-05-04 15:03:23 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        for (int i = 0; i < array.length; i++) {
+        /*Предполагаем, что первый элемент (в каждом
+           подмножестве элементов) является минимальным */
+            int min = array[i];
+            int min_i = i;
+        /*В оставшейся части подмножества ищем элемент,
+           который меньше предположенного минимума*/
+            for (int j = i; j < array.length; j++) {
+                //Если находим, запоминаем его индекс
+                if (array[j] > min) {
+                    min = array[j];
+                    min_i = j;
+                }
+            }
+        /*Если нашелся элемент, меньший, чем на текущей позиции,
+          меняем их местами*/
+            if (i != min_i) {
+                int tmp = array[i];
+                array[i] = array[min_i];
+                array[min_i] = tmp;
+            }
+        }
+    }
+}
